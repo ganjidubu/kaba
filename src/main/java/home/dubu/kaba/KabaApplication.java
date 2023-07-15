bf381c44703644a2157de2d5bd8fd2ec16113c29
ganjidubu <104jyj@hansung.ac.kr>
2023-07-15 22:22:41 +0900

    feat: 기본 설정 추가
    * 필요할거라고 생각하는 의존성 추가
      * jpa
      * h2
      * validation
      * web
      * webflux
      * lombok
      * configuration-processor

---
+++ src/main/java/home/dubu/kaba/KabaApplication.java
@@ -0,0 +1,13 @@
+package home.dubu.kaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KabaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KabaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/home/dubu/kaba/KabaApplicationTests.java
@@ -0,0 +1,13 @@
+package home.dubu.kaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KabaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
