c022f09430a27bc21b46d2b4e3ce7418bac2eb71
ganjidubu <104jyj@hansung.ac.kr>
2023-07-20 18:24:59 +0900

    feat: 검색 시 keyword 유효성 검사 추가

--- src/main/java/home/dubu/kaba/controller/SearchController.java
+++ src/main/java/home/dubu/kaba/controller/SearchController.java
@@ -4,11 +4,15 @@ import home.dubu.kaba.dto.response.PlaceSearchResponse;
 import home.dubu.kaba.dto.response.SearchListResponse;
 import home.dubu.kaba.service.SearchService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotBlank;
+
+@Validated
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/places/search")
@@ -17,7 +21,7 @@ public class SearchController {
 
 
     @GetMapping
-    public PlaceSearchResponse searchPlace(@RequestParam String keyword) {
+    public PlaceSearchResponse searchPlace(@RequestParam @NotBlank String keyword) {
         return searchService.searchByKeyword(keyword);
     }
 
